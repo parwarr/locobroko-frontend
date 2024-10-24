e6c7eeb30a9514d7bff50f5d9a23631142976e92
parwarr <parwar.habasch@gmx.de>
2024-10-24 21:26:05 +0200

    update styling and layout

--- src/components/ServerAddress/ServerAddress.jsx
+++ src/components/ServerAddress/ServerAddress.jsx
@@ -1,51 +1,53 @@
-import { FaDiscord, FaArrowRight } from 'react-icons/fa'; // Add icons for Discord and arrow
+import { FaDiscord, FaArrowRight, FaCopy, FaYoutube } from 'react-icons/fa'; 
 import Trailer from '../Trailer/Trailer';
 
 function ServerAddress() {
   return (
-    <div className='min-h-screen'>
-      <div className='pl-40 pt-14'>
-        <div className='flex flex-col p-10 text-left max-w-96 rounded-3xl bg-opacity-70 bg-slate-700 shadow-lg'>
-          <span className='bowlby-one-sc-regular text-6xl m-auto text-white'>
-            LocoBroko
-          </span>
-          <span className='text-wrap text-gray-400 my-6'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-            odio ad facilis quasi cumque.
-          </span>
-          <span className='bg-gradient-to-r from-blue-300 via-teal-500 bg-clip-text text-base font-extrabold text-transparent select-auto'>
-            SERVERADRESSE
-          </span>
-          <div className='flex flex-row mt-4'>
-            <div className='border border-transparent shadow-md shadow-[#6495ed] rounded-md w-52 p-2 bg-gradient-to-r from-blue-300 via-teal-500 bg-clip-text text-base font-extrabold text-transparent select-auto'>
-              LocoBroko.net
-            </div>
-            <button
-              onClick={() => navigator.clipboard.writeText('LocoBroko.net')}
-              className='border-transparent shadow-md shadow-[#6495ed] ease-in duration-300 rounded-md p-2 bg-[#6495ed] text-white hover:scale-110 hover:ring-4 hover:ring-[#6495ed] hover:ring-opacity-50 ml-[-10px]'
-            >
-              Kopieren
-            </button>
-          </div>
-        </div>
-        <div className='w-[90%] absolute right-[-20.75rem] transform -translate-y-1/2 p-4'>
+    <div className="min-h-screen flex items-center justify-center flex-col">
+<div className="flex flex-col p-6 md:p-10 text-left max-w-full md:max-w-lg rounded-3xl bg-opacity-70 bg-slate-700 shadow-lg mx-auto">
+  <span className="bowlby-one-sc-regular text-4xl md:text-6xl text-center text-white">
+    LocoBroko
+  </span>
+
+  <span className="text-gray-300 my-4 md:my-6 text-sm md:text-base text-center">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+    odio ad facilis quasi cumque.
+  </span>
+
+  <span className="text-gray-100 text-sm md:text-base font-extrabold text-center">
+    SERVERADRESSE
+  </span>
+
+  <div className="flex justify-center mt-4">
+    <div
+      onClick={() => navigator.clipboard.writeText('LocoBroko.net')}
+      className="flex items-center justify-between border border-gray-400 shadow-md rounded-md w-full md:w-52 p-2 text-sm md:text-base font-bold text-gray-200 bg-gray-800 cursor-pointer hover:bg-gray-700 transition-colors"
+    >
+      <span className="text-center">LocoBroko.net</span>
+      <FaCopy className="text-white ml-2" />
+    </div>
+  </div>
+</div>
+      <div className="flex flex-col items-center mt-8 md:mt-10 w-full">
+        <div className="w-[90%] md:w-[70%] mb-6">
           <Trailer />
         </div>
-        <div className='flex justify-center space-x-40'>
-          <a
-            href='#'
-            className='flex items-center bg-slate-700 bg-opacity-80 text-white text-xl py-2 px-6 rounded-lg shadow-lg hover:scale-105 hover:bg-slate-600 hover:border-t-white transition-transform'
-          >
-            <FaDiscord className='mr-3' /> Discord
-            <FaArrowRight className='ml-2' />
-          </a>
 
+        <div className="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-40">
           <a
-            href='#'
-            className='flex items-center bg-slate-700 bg-opacity-80 text-white text-xl py-2 px-6 rounded-lg shadow-lg hover:scale-105 hover:bg-slate-600 transition-transform'
+            href="https://discord.gg/fEEygAszUp"
+            className="flex items-center justify-center bg-slate-700 bg-opacity-80 text-white text-lg md:text-xl py-2 px-4 md:px-6 rounded-lg shadow-lg hover:scale-105 hover:bg-slate-600 transition-transform"
           >
-            <FaDiscord className='mr-3' /> Discord
-            <FaArrowRight className='ml-2' />
+            <FaDiscord className="mr-2" /> Discord
+            <FaArrowRight className="ml-2" />
+          </a>
+          
+          <a
+            href="https://www.youtube.com/channel/UCXTBHWIiYGXib9RZ5AwdHwA"
+            className="flex items-center justify-center bg-red-700 bg-opacity-80 text-white text-lg md:text-xl py-2 px-4 md:px-6 rounded-lg shadow-lg hover:scale-105 hover:bg-red-600 transition-transform"
+          >
+            <FaYoutube className="mr-2" /> Youtube
+            <FaArrowRight className="ml-2" />
           </a>
         </div>
       </div>
